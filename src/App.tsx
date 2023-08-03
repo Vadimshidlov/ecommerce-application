eb5ef5134a366e16cbec21717929262e296b42bf
Alexey <alexey.krupenia@gmail.com>
2023-08-03 20:46:56 +0300

    feat: EA-27. Create Header and integrate in project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import { Header } from "components/Header";
+
 function App() {
     return (
         <div>
+            <Header />
             <h1>Hello,React!</h1>
         </div>
     );
