e86d5dec26c02c0a79f518701ff8d024b60323a3
Vadimshidlov <mat23vadim@mail.ru>
2023-08-07 13:05:24 +0300

    feat: create token and products request

--- src/view/app-components/Button.tsx
+++ src/view/app-components/Button.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+type TokenResponseType = {
+    access_token: string;
+    expires_in: number;
+    token_type: string;
+    scope: string;
+    refresh_token: string;
+};
+
 async function sendRequest() {
     const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
+    const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
     const CTP_PROJECT_KEY = "uwoc_ecm-app";
     // const CTP_CLIENT_SECRET = "XP_DSn4dR1OJG4P8fpr9EUp0vZcSb-Ae";
     // const CTP_CLIENT_ID = "aP0M2mJZSI39Q4kSIVvg9knk";
@@ -9,7 +18,7 @@ async function sendRequest() {
     // const url = `https://auth.europe-west1.gcp.commercetools.com/oauth/uwoc_ecm-app/anonymous/token?grant_type=client_credentials&scope=create_anonymous_token:uwoc_ecm-app`;
     const url = `${CTP_AUTH_URL}/oauth/${CTP_PROJECT_KEY}/anonymous/token?grant_type=client_credentials&scope=view_published_products:uwoc_ecm-app`;
 
-    const token = await fetch(url, {
+    const tokenRequest = await fetch(url, {
         method: "POST",
         headers: {
             Authorization: `Basic YVAwTTJtSlpTSTM5UTRrU0lWdmc5a25rOlhQX0RTbjRkUjFPSkc0UDhmcHI5RVVwMHZaY1NiLUFl`,
@@ -18,7 +27,24 @@ async function sendRequest() {
         },
     });
 
-    console.log(await token.json());
+    const tokenResponse: TokenResponseType = await tokenRequest.json();
+    const token = tokenResponse.access_token;
+    console.log(tokenResponse);
+    console.log(token);
+
+    const urlProducts = `${CTP_API_URL}/${CTP_PROJECT_KEY}/product-projections`;
+    // const urlProductsPublished = `${CTP_API_URL}/${CTP_PROJECT_KEY}/product-projections&scope=view_published_products:${CTP_PROJECT_KEY}`;
+    // const urlProducts = `${CTP_API_URL}/${CTP_PROJECT_KEY}/products`;
+
+    const products = await fetch(urlProducts, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    console.log(await products.json());
 }
 
 export default function ButtonToken() {
