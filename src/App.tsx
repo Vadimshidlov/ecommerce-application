e349db5cebefffcd98101f4e24935bad20aa82b0
vitali007tut <vitali007@tut.by>
2023-08-10 16:00:50 +0300

    feat: start routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "view/app-components/Header/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div>
             <Header />
+            <Routes>
+                <Route path="/" element={<Header />} />
+                <Route path="about" element={<Header />} />
+                <Route path="contact" element={<Header />} />
+            </Routes>
             <h1>Hello,React!!!!</h1>
         </div>
     );
