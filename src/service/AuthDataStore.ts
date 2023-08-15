f3085c3847ad8298b286e225f32d559a38de0175
Vadimshidlov <mat23vadim@mail.ru>
2023-08-15 22:36:18 +0300

    feat: EA-15. Implemented AuthDataStore service

---
+++ src/service/AuthDataStore.ts
@@ -0,0 +1,68 @@
+export class AuthDataStore {
+    private static instance: AuthDataStore;
+
+    public static getAuthDataStore(): AuthDataStore {
+        if (!this.instance) {
+            this.instance = new AuthDataStore();
+        }
+
+        return this.instance;
+    }
+
+    public setAnonymousTokens(anonymousAccessToken: string, anonymousRefreshToken: string): void {
+        localStorage.setItem("anonymousAccessToken", anonymousAccessToken);
+        localStorage.setItem("anonymousRefreshToken", anonymousRefreshToken);
+    }
+
+    public setAuthTokens(accessAuthToken: string, refreshAuthToken: string): void {
+        localStorage.setItem("accessAuthToken", accessAuthToken);
+        localStorage.setItem("refreshAuthToken", refreshAuthToken);
+    }
+
+    public getAnonymousAccessToken(): string {
+        const anonymousAccessToken = localStorage.getItem("anonymousAccessToken");
+
+        if (!anonymousAccessToken) {
+            throw new Error("Anonymous access token was fallen");
+        }
+
+        return anonymousAccessToken;
+    }
+
+    public getAnonymousRefreshToken(): string {
+        const anonymousRefreshToken = localStorage.getItem("anonymousRefreshToken");
+
+        if (!anonymousRefreshToken) {
+            throw new Error("Anonymous refresh token was fallen");
+        }
+
+        return anonymousRefreshToken;
+    }
+
+    public getAccessAuthToken(): string {
+        const accessAuthToken = localStorage.getItem("accessAuthToken");
+
+        if (!accessAuthToken) {
+            throw new Error("Auth access token was fallen");
+        }
+
+        return accessAuthToken;
+    }
+
+    public getAuthRefreshToken(): string {
+        const refreshAuthToken = localStorage.getItem("refreshAuthToken");
+
+        if (!refreshAuthToken) {
+            throw new Error("Auth refresh token was fallen");
+        }
+
+        return refreshAuthToken;
+    }
+
+    public removeTokenFromStore(key: string): void {
+        const token = localStorage.getItem(key);
+        if (token) {
+            localStorage.removeItem(key);
+        }
+    }
+}
