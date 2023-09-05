0ff2bb8d0067c33d24697aa5b41ec29e8b37f077
vitali007tut <vitali007@tut.by>
2023-09-05 22:54:03 +0300

    feat: continue about

--- src/view/app-components/About/AboutPage.tsx
+++ src/view/app-components/About/AboutPage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import "view/app-components/About/style.scss";
 import { ReactComponent as ReactLogo } from "assets/svg/rs_school.svg";
 import { ReactComponent as GithubLogo } from "assets/svg/github-mark.svg";
@@ -12,15 +12,25 @@ enum Person {
     Vitali = "vitali007tut",
 }
 
-export default function AboutPage() {
+function AboutPage() {
+    const [people, setPeople] = useState([
+        { id: 1, name: "verv", image: "servev" },
+        { id: 2, name: "sbrtb", image: "2222v" },
+        { id: 3, name: "NAME333", image: "3IMAGEv33" },
+    ]);
+
     return (
         <div className="about-container">
-            <PersonItem />
-            <div>AboutPage</div>
-            <GithubLogo className="github-logo" />
+            <div className="team-container">
+                {people.map((person) => (
+                    <PersonItem {...person} />
+                ))}
+            </div>
             <Link to="https://rs.school" target="_blank">
                 <ReactLogo className="rs-logo" width="200" />
             </Link>
         </div>
     );
 }
+
+export default AboutPage;
--- src/view/app-components/About/PersonItem.tsx
+++ src/view/app-components/About/PersonItem.tsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
+import { ReactComponent as GithubLogo } from "assets/svg/github-mark.svg";
+import { Link } from "react-router-dom";
 
-// type Props = {
-//     name: string;
-//     image: string;
-// };
+type Props = {
+    name: string;
+    image: string;
+};
 
-export default function PersonItem() {
+function PersonItem(props: Props) {
+    console.log(props);
+
     return (
-        <div>
-            <div>props</div>
-            <div>PersonItem</div>
+        <div className="person">
+            <div>Image{props.image}</div>
+            <div>Name{props.name}</div>
+            <div>Role</div>
+            <div>Bio</div>
+            <GithubLogo className="github-logo" />
         </div>
     );
 }
+
+export default PersonItem;
